ef649b61554c04bc7744472407bd611249628d82
nintencoin <nintencoin@gmail.com>
2014-01-23 12:31:46 +0000

    Updates

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -10,22 +10,22 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
     QSplashScreen(pixmap, f)
 {
     // set reference point, paddings
-    int paddingLeftCol2         = 230;
-    int paddingTopCol2          = 376;
-    int line1 = 0;
-    int line2 = 13;
-    int line3 = 26;
+    //int paddingLeftCol2         = 230;
+    //int paddingTopCol2          = 376;
+    //int line1 = 0;
+    //int line2 = 13;
+    //int line3 = 26;
 
-    float fontFactor            = 1.0;
+    //float fontFactor            = 1.0;
 
     // define text to place
     QString titleText       = QString(QApplication::applicationName()).replace(QString("-testnet"), QString(""), Qt::CaseSensitive); // cut of testnet, place it as single object further down
     QString versionText     = QString("Version %1 ").arg(QString::fromStdString(FormatFullVersion()));
-    QString copyrightText1   = QChar(0xA9)+QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Bitcoin developers"));
-    QString copyrightText1   = QChar(0xA9)+QString(" 2011-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Litecoin developers"));
-    QString copyrightText3   = QChar(0xA9)+QString(" 2014 ") + QString(tr("The Nintencoin developers"));
+    //QString copyrightText1   = QChar(0xA9)+QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Bitcoin developers"));
+    //QString copyrightText2   = QChar(0xA9)+QString(" 2011-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Litecoin developers"));
+    //QString copyrightText3   = QChar(0xA9)+QString(" 2014 ") + QString(tr("The Nintencoin developers"));
 
-    QString font            = "Arial";
+    //QString font            = "Arial";
 
     // load the bitmap for writing some text over it
     QPixmap newPixmap;
